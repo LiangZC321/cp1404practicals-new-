048a9e42f31e95e7a07ec3d987f41a0c899e0ae0
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-05-30 21:22:19 +0800

    the same function to evaluate the score

---
+++ prac_02/score_menu.py
@@ -0,0 +1,9 @@
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
