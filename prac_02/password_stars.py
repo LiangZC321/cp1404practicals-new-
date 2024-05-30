4f180e890175eab9811b11f4f5c06b17c0cd8fc4
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-05-30 20:34:19 +0800

    get password function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,3 +1,13 @@
 def main():
     password = get_password()
     print_password_asterisks(password)
+
+def get_password():
+    min_length = 7
+    password = input("Please enter your password:")
+
+    while len(password) < min_length:
+        print("Invalid password. At least 7 characters long.")
+        password = input("Please enter your password:")
+
+    return password
