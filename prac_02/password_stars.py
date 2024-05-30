aca763fc5dc8879b21d295fadd4e3daa91471ddd
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-05-30 20:46:08 +0800

    rebulid it according to Module docstring

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,13 @@
+MIN_LENGTH = 7
+
 def main():
     password = get_password()
     print_password_asterisks(password)
 
 def get_password():
-    min_length = 7
     password = input("Please enter your password:")
 
-    while len(password) < min_length:
+    while len(password) < MIN_LENGTH:
         print("Invalid password. At least 7 characters long.")
         password = input("Please enter your password:")
 
