5f2806fe48a56e555ffded0a002acaa10b97ffc8
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-05-30 20:09:10 +0800

    add files prac01

---
+++ prac_01/sales_bonus.py
@@ -0,0 +1,25 @@
+"""
+get sales
+while sales >= 0
+    calculate bonus (this line is intentionally incomplete pseudocode)
+    print bonus
+    get sales
+do next thing
+"""
+
+
+LOW_SALES_THRESHOLD = 1000
+LOW_SALES_BONUS_RATE = 0.1
+HIGH_SALES_BONUS_RATE = 0.15
+
+sales = float(input("Enter sales: $"))
+
+if sales >= 0:
+    if sales < LOW_SALES_THRESHOLD:
+        bonus = sales * LOW_SALES_BONUS_RATE
+    else:
+        bonus = sales * HIGH_SALES_BONUS_RATE
+    print("Bonus: $", bonus)
+else:
+    print("Invalid sales amount.")
+
