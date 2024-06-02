58c83cb5e1cb7ea58ca7c78f9c298bf20269754b
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-03 06:55:12 +0800

    read the file

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,3 +2,9 @@ name = input("Please enter your name: ")
 file = open("name.txt", "w")
 file.write(name)
 file.close()
+
+
+file = open("name.txt", "r")
+name = file.read().strip()
+file.close()
+print(f"Hi {name}!")
