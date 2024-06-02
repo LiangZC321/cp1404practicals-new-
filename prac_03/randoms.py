e91901be5488547d5cef6a05737a4eedceebec61
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-03 05:44:16 +0800

    made the answers

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,5 +1,26 @@
 """
+output is:
 7
 5
 2.9789130507143513
+
+What did you see on line 1?
+What was the smallest number you could have seen, what was the largest?
+7
+Smallest number: 5
+Largest number: 20
+
+What did you see on line 2?
+What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4?
+5
+Smallest number: 3
+Largest number: 9
+No
+
+What did you see on line 3?
+What was the smallest number you could have seen, what was the largest?
+2.9789130507143513
+Smallest number: 2.5
+Largest number: 5.5
 """
