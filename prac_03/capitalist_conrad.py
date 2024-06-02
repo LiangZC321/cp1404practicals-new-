e66a28693148d6c410738fdaae4958c02979f08a
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-03 06:16:32 +0800

    output to a file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -19,10 +19,13 @@ MAX_DECREASE = 0.05
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "prices.txt"
 
 price = INITIAL_PRICE
-number_of_days = 0 #variable
+number_of_days = 0
+out_file = open(FILENAME, 'w')
 print(f"Starting price: ${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -40,3 +43,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
 
     price *= (1 + price_change)
     print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
