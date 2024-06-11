923d0b5f9bde1dea6f5ec320997522312ee8f24d
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-11 11:05:42 +0800

    output information about these numbers

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,20 @@
+numbers = []
+
+for i in range(5):
+    number = int(input(f"Enter number {i + 1}: "))
+    numbers.append(number)
+
+print("\nYou entered these numbers:")
+for num in numbers:
+    print(f"Number: {num}")
+
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+
+
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+
+average = sum(numbers) / len(numbers)
+print(f"The average of the numbers is {average:.1f}")
