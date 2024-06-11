2afbf964dd25b9e8f764b31a7962340f272bfa9e
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-11 10:42:27 +0800

    function for display

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,12 +9,13 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_new_data()
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     teachers_information = []
-    input_file = open("subject_data", 'r')
+    input_file = open(FILENAME, 'r')
     for line in input_file:
         # print(line)  # See what a line looks like
         # print(repr(line))  # See what a line really looks like
@@ -27,6 +28,15 @@ def get_data():
         # print("----------")
     input_file.close()
     return teachers_information
+
+
+def display_new_data():
+    input_file = open(FILENAME, 'r')
+    for line in input_file:
+        line = line.strip()
+        parts = line.split(',')
+        print(f"{parts[0]} is taught by {parts[1]}and has {parts[2]} students")
+    input_file.close()
 
 
 main()
