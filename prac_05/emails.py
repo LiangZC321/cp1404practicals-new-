976316b636cdb24d8f91bd082405fadb778a9dba
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-19 10:53:28 +0800

    structure for main function

---
+++ prac_05/emails.py
@@ -0,0 +1,10 @@
+NAME_AND_EMAIL = {}
+
+def main():
+    email = input("Email: ")
+    check_email(email)
+    while email != "":
+        check_email(email)
+        user_name = get_user_name(email)
+        check_user_name(user_name, email)
+        email = input("Email: ")
--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,3 +5,8 @@ WORD_TO_COUNT = {}
 for word in texts:
     word =word.lower()
     WORD_TO_COUNT[word] = WORD_TO_COUNT.get(word, 0) + 1
+
+max_word_length = max(len(word) for word in WORD_TO_COUNT.keys())
+print("Text: ")
+for word, count in sorted(WORD_TO_COUNT.items()):
+    print(f"{word:<{max_word_length}}: {count}")
