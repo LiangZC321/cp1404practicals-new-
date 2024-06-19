a8163a5713470d6044849ce89c9d01966b2ea43f
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-19 11:36:16 +0800

    information from file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,3 +6,11 @@ def main():
     information = read_file(filename)
     champions_to_times, champion_countries = organize_information(information)
     display_summy(champions_to_times, champion_countries)
+
+def read_file(filename):
+    information = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        reader = csv.reader(in_file)
+        for row in reader:
+            information.append(row)
+    return information
