826f0cdc4f102969bd9dd81e5f2c7fa41f4d2fd8
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-06-19 10:42:17 +0800

    similar loop

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -9,3 +9,12 @@ COLOR_TO_CODE = {"Absolute Zero": "#0048ba",
                  "Beaver": "#9f8170",
                  "Beige": "#f5f5dc",
                  }
+
+color_name = input("Enter the color name: ").title()
+while color_name != "":
+    if color_name not in COLOR_TO_CODE.keys():
+        print("Invalid color name")
+        color_name = input("Enter the color name: ").title()
+    else:
+        print(f"The {color_name} corresponds to code {COLOR_TO_CODE[color_name]}.")
+    color_name = input("Enter the color name: ").title()
