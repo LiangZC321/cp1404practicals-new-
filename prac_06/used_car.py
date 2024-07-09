0e69eba1253caa2182cf22c9d1275d2d6788ff60
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-07-09 10:50:48 +0800

    limo, print fuel

--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -14,5 +14,10 @@ def main():
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
+    limo = Car(100)
+    limo.add_fuel(20)
+    print(f"Limo has fuel: {limo.fuel}")
+    print(limo)
+
 
 main()
