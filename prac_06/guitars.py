5fc0460f3802749bcb3e4185202c9e43a93779b1
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-07-09 12:31:49 +0800

    testing with lines

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,16 +4,18 @@ from guitar import Guitar
 def main():
     guitars = []
 
-    print("My guitars!")
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: "))
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-        print(f"{guitar} added.\n")
-        name = input("Name: ")
+    # print("My guitars!")
+    # name = input("Name: ")
+    # while name != "":
+    #     year = int(input("Year: "))
+    #     cost = float(input("Cost: "))
+    #     guitar = Guitar(name, year, cost)
+    #     guitars.append(guitar)
+    #     print(f"{guitar} added.\n")
+    #     name = input("Name: ")
 
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     if guitars:
         print("\nThese are my guitars:")
         for i, guitar in enumerate(guitars, 1):
