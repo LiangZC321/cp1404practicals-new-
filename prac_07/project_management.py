f4bf3f7f8ba4b2eac989f4405e3c48b7eb883f43
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-07-18 22:58:52 +0800

    import and menu

---
+++ prac_07/project_management.py
@@ -0,0 +1,5 @@
+import csv
+from project import Project
+from datetime import datetime
+
+menu = "(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by date\n(A)dd new project\n(U)pdate project\n(Q)uit"
