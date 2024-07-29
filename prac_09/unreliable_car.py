6b83afddf6d77b0ad0a5cd780b034d727582a27b
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-07-29 13:30:13 +0800

    Only drive if the random number is less than the reliability

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -6,3 +6,8 @@ class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        if random.random() * 100 < self.reliability:
+            return super().drive(distance)
+        return 0
