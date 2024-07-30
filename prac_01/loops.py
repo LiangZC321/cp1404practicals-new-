d2b7d0e09fe0bbaaea793ac1114f770ef9f144f3
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-07-30 09:51:45 +0800

    redo

--- prac_01/loops.py
+++ prac_01/loops.py
@@ -1,27 +1,42 @@
-for i in range(1, 21, 2):
-    print(i, end=' ')
-print()
-# (Question a)
-print("Question a:")
-for i in range(0, 101, 10):
-    print(i, end=' ')
-print()
+# for i in range(1, 21, 2):
+#     print(i, end=' ')
+# print()
+# # (Question a)
+# print("Question a:")
+# for i in range(0, 101, 10):
+#     print(i, end=' ')
+# print()
+#
+# # (Question b)
+# print("\nQuestion b:")
+# for i in range(20, 0, -1):
+#     print(i, end=' ')
+# print()
+#
+# # (Question c)
+# print("\nQuestion c:")
+# num_stars = int(input("Number of stars: "))
+# for _ in range(num_stars):
+#     print('*', end='')
+# print()
+#
+# # （Question d）
+# print("\nQuestion d:")
+# num_stars = int(input("Number of stars: "))
+# for i in range(1, num_stars + 1):
+#     print('*' * i)
 
-# (Question b)
-print("\nQuestion b:")
+for i in range(1, 20, 2):
+    print(i, end=" ")
+for i in range(0, 101, 10):
+    print(i, end=" ")
+print()
 for i in range(20, 0, -1):
     print(i, end=' ')
+
 print()
 
-# (Question c)
-print("\nQuestion c:")
-num_stars = int(input("Number of stars: "))
-for _ in range(num_stars):
-    print('*', end='')
-print()
-
-# （Question d）
-print("\nQuestion d:")
-num_stars = int(input("Number of stars: "))
+num_stars = int(input("how many *: "))
+print(num_stars * "*")
 for i in range(1, num_stars + 1):
-    print('*' * i)
+    print("*" * i)
--- prac_01/sales_bonus.py
+++ prac_01/sales_bonus.py
@@ -6,20 +6,35 @@ while sales >= 0
     get sales
 do next thing
 """
-
-
+#
+#
+# LOW_SALES_THRESHOLD = 1000
+# LOW_SALES_BONUS_RATE = 0.1
+# HIGH_SALES_BONUS_RATE = 0.15
+#
+# sales = float(input("Enter sales: $"))
+#
+# if sales >= 0:
+#     if sales < LOW_SALES_THRESHOLD:
+#         bonus = sales * LOW_SALES_BONUS_RATE
+#     else:
+#         bonus = sales * HIGH_SALES_BONUS_RATE
+#     print("Bonus: $", bonus)
+# else:
+#     print("Invalid sales amount.")
 LOW_SALES_THRESHOLD = 1000
 LOW_SALES_BONUS_RATE = 0.1
 HIGH_SALES_BONUS_RATE = 0.15
-
-sales = float(input("Enter sales: $"))
+sales = float(input("Enter sales: "))
 
 if sales >= 0:
-    if sales < LOW_SALES_THRESHOLD:
-        bonus = sales * LOW_SALES_BONUS_RATE
-    else:
+    if sales >= LOW_SALES_THRESHOLD:
         bonus = sales * HIGH_SALES_BONUS_RATE
-    print("Bonus: $", bonus)
+
+    else:
+        bonus = sales * LOW_SALES_BONUS_RATE
+    print(f"Bonus is {bonus}")
+
 else:
-    print("Invalid sales amount.")
+    print("INVALID")
 
