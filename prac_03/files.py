1be7e1b9511f2b0156d6e0ef16d919e1acce227d
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-08-01 09:20:05 +0800

    redo

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,24 +1,42 @@
-name = input("Please enter your name: ")
-file = open("name.txt", "w")
+# name = input("Please enter your name: ")
+# file = open("name.txt", "w")
+# file.write(name)
+# file.close()
+#
+#
+# file = open("name.txt", "r")
+# name = file.read().strip()
+# file.close()
+# print(f"Hi {name}!")
+#
+#
+# with open("numbers.txt", "r") as file:
+#     first_number = int(file.readline().strip())
+#     second_number = int(file.readline().strip())
+# result = first_number + second_number
+# print(result)
+#
+# total = 0
+# with open("numbers.txt", "r") as file:
+#     for line in file:
+#         total += int(line.strip())
+#
+# print(total)
+
+filename = "name.txt"
+file = open(filename, "w")
+name = input("ur name?")
 file.write(name)
 file.close()
 
-
-file = open("name.txt", "r")
+file = open(filename, "r")
 name = file.read().strip()
+print(f"hello {name}")
 file.close()
-print(f"Hi {name}!")
-
-
-with open("numbers.txt", "r") as file:
-    first_number = int(file.readline().strip())
-    second_number = int(file.readline().strip())
-result = first_number + second_number
-print(result)
 
 total = 0
 with open("numbers.txt", "r") as file:
+    line = file.readline()
     for line in file:
         total += int(line.strip())
-
-print(total)
+    print(total)
