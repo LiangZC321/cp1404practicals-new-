a14ab8bc1ad2ff6a35ba816d3269dbdd06ad4200
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-08-05 09:41:55 +0800

    test cars fuel

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -47,6 +47,17 @@ def run_tests():
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    def test_car_fuel():
+        car_with_fuel = Car('WithFuel', 10)
+        assert car_with_fuel.fuel == 10, "Initial fuel not set correctly."
+
+        car_without_fuel = Car('WithoutFuel')
+        assert car_without_fuel.fuel == 0, "Default fuel should be 0."
+
+        print("All test cases passed for Car fuel property")
+
+
+    test_car_fuel()
 
 
 run_tests()
