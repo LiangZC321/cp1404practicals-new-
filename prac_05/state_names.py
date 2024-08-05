c25074a85f87fc1e310150acf26bba6cf767773a
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-08-05 11:08:25 +0800

    redo

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,24 +4,50 @@ State names in a dictionary
 File needs reformatting
 """
 
-#Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland",
-                "NSW": "New South Wales",
-                "NT": "Northern Territory",
-                "WA": "Western Australia",
-                "ACT": "Australian Capital Territory",
-                "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+# #Reformat this file so the dictionary code follows PEP 8 convention
+# CODE_TO_NAME = {"QLD": "Queensland",
+#                 "NSW": "New South Wales",
+#                 "NT": "Northern Territory",
+#                 "WA": "Western Australia",
+#                 "ACT": "Australian Capital Territory",
+#                 "VIC": "Victoria", "TAS": "Tasmania"}
+# print(CODE_TO_NAME)
+#
+# for code, name in CODE_TO_NAME.items():
+#     print(f"{code:<3} is {name}")
+#
+# state_code = input("Enter short state: ").upper()
+# while state_code != "":
+#     try:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     except KeyError:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
 
-for code, name in CODE_TO_NAME.items():
-    print(f"{code:<3} is {name}")
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+for code in CODE_TO_NAME:
+    print(f"{code:3} is {CODE_TO_NAME[code]:>25}")
 
 state_code = input("Enter short state: ").upper()
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ")
+
 while state_code != "":
     try:
         print(state_code, "is", CODE_TO_NAME[state_code])
     except KeyError:
-        print("Invalid short state")
+        print("Invalid code")
     state_code = input("Enter short state: ").upper()
-
-
