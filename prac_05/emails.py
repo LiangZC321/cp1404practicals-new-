90884bec08e358ad22a1f5c0d5f4ddf482e1b047
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-08-05 12:38:07 +0800

    redo

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,41 +1,65 @@
-NAME_AND_EMAIL = {}
+# NAME_AND_EMAIL = {}
+#
+# def main():
+#     email = input("Email: ")
+#     check_email(email)
+#     while email != "":
+#         check_email(email)
+#         user_name = get_user_name(email)
+#         check_user_name(user_name, email)
+#         email = input("Email: ")
+#
+#     for name, email in NAME_AND_EMAIL.items():
+#         print(f"{name.title()} ({email})")
+#
+# def check_email(email):
+#     while "@" not in email:
+#         print("Invalid email")
+#         email = input("Email: ")
+#         return email
+#     return email
+#
+# def get_user_name(email):
+#     users_information = email.split("@")[0].split('.')
+#     user_name = ' '.join(information.title() for information in users_information)
+#     return user_name
+#
+# def check_user_name(user_name, email):
+#     name_confirmation = input(f"Is your name {user_name}?（Y/n) ").lower()
+#     while name_confirmation == "n":
+#         user_name = input("Name: ")
+#         key = f"{user_name}"
+#         value = f"{email}"
+#         NAME_AND_EMAIL[key] = value
+#         return NAME_AND_EMAIL
+#     key = f"{user_name}"
+#     value = f"{email}"
+#     NAME_AND_EMAIL[key] = value
+#     return NAME_AND_EMAIL
+#
+#
+# main()
 
-def main():
-    email = input("Email: ")
-    check_email(email)
-    while email != "":
-        check_email(email)
-        user_name = get_user_name(email)
-        check_user_name(user_name, email)
-        email = input("Email: ")
+email_to_name ={}
 
-    for name, email in NAME_AND_EMAIL.items():
-        print(f"{name.title()} ({email})")
-
-def check_email(email):
-    while "@" not in email:
+email = input("enter the email: ")
+while email != "":
+    if "@" not in email:
         print("Invalid email")
-        email = input("Email: ")
-        return email
-    return email
-
-def get_user_name(email):
-    users_information = email.split("@")[0].split('.')
-    user_name = ' '.join(information.title() for information in users_information)
-    return user_name
-
-def check_user_name(user_name, email):
-    name_confirmation = input(f"Is your name {user_name}?（Y/n) ").lower()
-    while name_confirmation == "n":
-        user_name = input("Name: ")
-        key = f"{user_name}"
-        value = f"{email}"
-        NAME_AND_EMAIL[key] = value
-        return NAME_AND_EMAIL
-    key = f"{user_name}"
-    value = f"{email}"
-    NAME_AND_EMAIL[key] = value
-    return NAME_AND_EMAIL
+        email = input("enter the email: ")
+    else:
+        emails = email.split('@')
+        parts = emails[0].split('.')
+        name = ' '.join(parts).title()
+        choice = input(f"is the name {name} Y/n?")
+        if choice == "" or choice == "y":
+            email_to_name[email] = name
+        else:
+            name = input("enter the name: ")
+            email_to_name[email] = name
+        email = input("enter the email: ")
 
 
-main()
+for email in email_to_name:
+    print(f"{email_to_name[email]} ({email})")
+
