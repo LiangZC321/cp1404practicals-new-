7a2607da1d5d7c17583c70100c4f3794eae25c04
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-08-05 11:18:04 +0800

    redo

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -10,11 +10,20 @@ COLOR_TO_CODE = {"Absolute Zero": "#0048ba",
                  "Beige": "#f5f5dc",
                  }
 
-color_name = input("Enter the color name: ").title()
-while color_name != "":
-    if color_name not in COLOR_TO_CODE.keys():
-        print("Invalid color name")
-        color_name = input("Enter the color name: ").title()
-    else:
-        print(f"The {color_name} corresponds to code {COLOR_TO_CODE[color_name]}.")
-    color_name = input("Enter the color name: ").title()
+
+# color_name = input("Enter the color name: ").title()
+# while color_name != "":
+#     if color_name not in COLOR_TO_CODE.keys():
+#         print("Invalid color name")
+#         color_name = input("Enter the color name: ").title()
+#     else:
+#         print(f"The {color_name} corresponds to code {COLOR_TO_CODE[color_name]}.")
+#     color_name = input("Enter the color name: ").title()
+
+color = input("color name: ").title()
+while color != "":
+    try:
+        print(f"{color} is {COLOR_TO_CODE[color]}")
+    except KeyError:
+        print("invalid color")
+    color = input("color name: ").title()
